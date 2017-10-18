a6a122ba5e607cec43156af266edbaa4b0403267
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-18 09:52:55 -0400

    test parent/child relationship with app and comment box components

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,21 +3,16 @@ import App from '../../src/components/app';
 
 // Use describe to group together similar tests/typical mocha set up
 describe('App' , () => {
+  let component;
+
+  beforeEach( () => {
+    component = renderComponent(App);
+  });
 
   // Use 'it' to test a single attribute of a target, 'it' blocks try to make
   // an assertion about a particular fact about the testing subject
-    it('shows the correct text', () => {
-
-      // create an instance of App
-      const component = renderComponent(App);
-
+    it('shows a comment box', () => {
       // Use 'expect' to make an assertion/belief about a target
-      expect(component).to.contain('React simple starter');
-
-
-
-
+      expect(component.find('.comment-box')).to.exist;
   });
-
-
 });
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,27 +3,25 @@ import CommentBox from '../../src/components/comment_box';
 
 // Use describe to group together similar tests/typical mocha set up
 describe ('CommentBox' , () => {
+  let component;
+
+  beforeEach( () => {
+      component = renderComponent(CommentBox);
+  });
+
+  it('has the correct class' , () => {
+    expect(component).to.have.class('comment-box');
+  });
 
   // Use 'it' to test a single attribute of a target, 'it' blocks try to make
   // an assertion about a particular fact about the testing subject
   it('has a text area' , () => {
-
-      // create an instance of CommentBox
-    const component = renderComponent(CommentBox);
-
     // Use 'expect' to make an assertion/belief about a target
     // here is where chai jquery methods come in; helps for making assertions
     expect(component.find('textarea')).to.exist;
-
   });
 
   it('has a button' , () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
-
   });
-
-
-
-
 });
