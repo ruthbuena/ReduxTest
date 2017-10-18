23637a9d3ae3604b53720e7da6ed8458c4b216fe
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-18 08:34:58 -0400

    add test component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -10,7 +10,8 @@ describe('App' , () => {
   });
 
 // Use 'it' to test a single attribute of a target
-  it('renders something', () => {
+  it('shows the correct text', () => {
+// Use 'expect' to make an assertion/belief about a target
     expect(component).to.exist;
   });
 });
