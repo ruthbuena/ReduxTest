6f0d0e409bce8ddc9211efd89409e26fe33fa204
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-18 13:33:43 -0400

    add h4 header and div to separate box & button in comment_box component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import commentsReducer from './comments';
 
 const rootReducer = combineReducers({
-  comments: commentReducer
+  comments: commentsReducer
 });
 
 export default rootReducer;
