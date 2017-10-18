696997248c0856323b6b0260f015e1424f0abbd2
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-18 08:41:31 -0400

    basic test syntax describe, it, expect

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,16 +3,17 @@ import App from '../../src/components/app';
 
 // Use describe to group together similar tests
 describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  // Use 'it' to test a single attribute of a target, 'it' blocks try to make
+  // an assertion about a particular fact about the testing subject
+    it('shows the correct text', () => {
+  // Use 'expect' to make an assertion/belief about a target
+      expect(component).to.exist;
+
+
+
+
   });
 
-// Use 'it' to test a single attribute of a target, 'it' blocks try to make
-// an assertion about a particular fact about the testing subject
-  it('shows the correct text', () => {
-// Use 'expect' to make an assertion/belief about a target
-    expect(component).to.exist;
-  });
+
 });
